925866d9e24d2695c717e7ce51b91295cd0514e9
gayane-tumanyan-86 <tumanjan.gug@mail.ru>
2021-10-08 23:07:05 +0400

    figure

--- src/chapter3/TwoDAgain.java
+++ src/chapter3/TwoDAgain.java
@@ -3,7 +3,10 @@ package chapter3;
 public class TwoDAgain {
     public static void main(String[] args) {
         int twoD[][]=new int [4][];
-        twoD [0]=new int[1];
+        for (int i : twoD[0] = new int[1]) {
+            
+        }
+
         twoD [1]=new int [2];
         twoD [2]=new int [3];
         twoD [3]=new int [4];
---
+++ src/homework/FigurePainter.java
@@ -0,0 +1,27 @@
+package homework;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+        /**
+         * Գրել կոդ, որը կպատկերի այս եռանկյունը։
+         *
+         *      *
+         *      * *
+         *      * * *
+         *      * * * *
+         *
+         * */
+
+
+              for (int i = 0; i < 4; i++){
+                  for (int j = 0; j < i+0; j++) {
+
+            System.out.print("* " );}
+                  System.out.println("* ");
+            }}}
+
+
+
+
+
+
