6ad03e4ef3525a898e99a5a4a3ef9882d08ce3d2
gayane-tumanyan-86 <tumanjan.gug@mail.ru>
2021-10-09 23:09:04 +0400

    figure2

--- src/homework/FigurePainter3.java
+++ src/homework/FigurePainter3.java
@@ -14,13 +14,15 @@ public class FigurePainter3 {
          *
          * */
 
-        for (int i = 4; i >=1; i--) {
-            for (int j =i-1; j >= i; j--) {
-                System.out.print(" ");}
-                for (int k =1; k <=i; k++) {
-                 System.out.print("*");
-                }
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
 
+                System.out.print(" ");
+            }
+            for (int j = i; j <=4; j++) {
+                System.out.print("*");
+            }
             System.out.println(" ");
         }
-    }}
+    }
+}
