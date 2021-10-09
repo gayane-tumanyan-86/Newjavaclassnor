e84976df65e01b4dfa57dfb5e17e5a451d457b8c
gayane-tumanyan-86 <tumanjan.gug@mail.ru>
2021-10-09 23:14:17 +0400

    figure2

--- src/homework/FigurePainter1.java
+++ src/homework/FigurePainter1.java
@@ -17,7 +17,7 @@ public class FigurePainter1 {
 
                 System.out.print(" ");
             }
-            for (int k = 1; k <= i; k++) {
+            for (int  j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println(" ");
